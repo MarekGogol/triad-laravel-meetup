589e3a14bf6019c59408ed9312d9e5504772aed2
Marek Gogol <9mike66@gmail.com>
2020-04-16 16:25:14 +0200

    added translatable static texts into template

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -5,5 +5,10 @@
     <!-- ARTICLE -->
     <h1>{{ $article->name }}</h1>
     {!! $article->content !!}
+    <hr>
+    <h2>{{ _('Hardcoded texts in template') }}</h2>
+    <p>{{ _('This is my first static text') }}</p>
+    <p>{{ _('Second static text') }}</p>
+    <p>{{ _('Third static text') }}</p>
 </div>
 @stop
